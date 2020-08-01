b1c961d52954148c02a6318289253c88a06a0325
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-08-01 17:41:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,34 @@ const wakeDog= function(dogName, dogBreed)
 {
   console.log(`Wake ${dogName} the ${dogBreed}`);
   return `Wake ${dogName} the ${dogBreed}`;
-}
+};
+
+const leashDog= function(dogName, dogBreed)
+{
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+  return `Wake ${dogName} the ${dogBreed}`;
+};
+
+const walkToPark= function(dogName, dogBreed)
+{
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+  return `Wake ${dogName} the ${dogBreed}`;
+};
+
+const throwFrisbee= function(dogName, dogBreed)
+{
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+  return `Wake ${dogName} the ${dogBreed}`;
+};
+
+const walkHome= function(dogName, dogBreed)
+{
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+  return `Wake ${dogName} the ${dogBreed}`;
+};
+
+const unleashDog= function(dogName, dogBreed)
+{
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+  return `Wake ${dogName} the ${dogBreed}`;
+};
